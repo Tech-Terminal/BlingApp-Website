709305cc5879424b45261c55ea99ab4e580b3ef3
aml Walaed <amlWalaed@gmail.com>
2026-09-07 21:43:00 +0300

    Update Nuxt configuration to include @nuxt/image module and add dependency in package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxt/image"],
   css: ["~/assets/css/index.css"],
   app: {
     head: {
