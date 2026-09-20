d7e8de00099615ecd7b5b77486ff609475c97438
aml Walaed <amlWalaed@gmail.com>
2026-09-20 07:10:37 +0300

    Update login page and API to reflect 'signIn' terminology; enhance localization for titles, descriptions, and placeholders to improve user experience and consistency across components.

--- server/utils/api.ts
+++ server/utils/api.ts
@@ -3,11 +3,15 @@ import type { H3Event } from "h3";
 const createApiClient = (event: H3Event) => {
   const config = useRuntimeConfig(event);
 
+  const locale = getCookie(event, "user-locale") ?? "ar";
+
   return $fetch.create({
     baseURL: config.backendBase,
 
     headers: {
-      // common headers
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Accept-Language": locale,
     },
   });
 };
